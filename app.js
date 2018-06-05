0c14281387ce2729774a89edc3bd105855ab4c98
Chirag <chirag.krishnaswamy@gmail.com>
2018-06-05 23:38:39 +0530

    * Updated header.ejs with a previous version of bootstrap (3.3.7)   in order to avoid syntactical conflit with tutorial. * Updated campgrounds.ejs : styled using bootstap (inline styling,   no seperate .css file yet.) * Updated app.js with some more camps to have more data to view   and check if grid displays correctly.

--- app.js
+++ app.js
@@ -8,7 +8,12 @@ app.set("view engine", "ejs");
 var campgrounds = [
         {name: "Salmon Creek", image: "https://pixabay.com/get/ec31b90f2af61c22d2524518b7444795ea76e5d004b014439df2c079a3e8b4_340.jpg"},
         {name: "Loard Hill", image: "https://pixabay.com/get/e837b1072af4003ed1584d05fb1d4e97e07ee3d21cac104497f8c378a7e9b0b9_340.jpg"},
-        {name: "Pitts Rest", image: "https://farm2.staticflickr.com/1363/1342367857_2fd12531e7.jpg"}
+        {name: "Pitts Rest", image: "https://farm2.staticflickr.com/1363/1342367857_2fd12531e7.jpg"},
+        {name: "Collin Woods", image: "https://farm2.staticflickr.com/1281/4684194306_18ebcdb01c.jpg"},
+        {name: "Pitts Rest", image: "https://farm2.staticflickr.com/1363/1342367857_2fd12531e7.jpg"},
+        {name: "Salmon Creek", image: "https://pixabay.com/get/ec31b90f2af61c22d2524518b7444795ea76e5d004b014439df2c079a3e8b4_340.jpg"},
+        {name: "Collin Woods", image: "https://farm2.staticflickr.com/1281/4684194306_18ebcdb01c.jpg"},
+        {name: "Loard Hill", image: "https://pixabay.com/get/e837b1072af4003ed1584d05fb1d4e97e07ee3d21cac104497f8c378a7e9b0b9_340.jpg"}
     ];
 
 // add routes below
