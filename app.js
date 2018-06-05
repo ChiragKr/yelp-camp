b0637338828f64cb1da76cfe09d3051a6e792c7a
Chirag <chirag.krishnaswamy@gmail.com>
2018-06-05 22:52:27 +0530

    * Added a POST route in app.ejs to send post   request for adding new campground   (temporarily. database not added yet) * Created "views/new.ejs" a page to display form   to add new campground (and make the POST request) * Added route to "/campgrounds/new" in app.js * Added anchor tag in "/campgrounds" to "/campgrounds/new" * Added anchor tag in "/campgrounds/new" to "/campgrounds"

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 var express = require("express");
 var app = express();
+var bodyParser = require("body-parser");
 
-
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+
+var campgrounds = [
+        {name: "Salmon Creek", image: "https://pixabay.com/get/ec31b90f2af61c22d2524518b7444795ea76e5d004b014439df2c079a3e8b4_340.jpg"},
+        {name: "Loard Hill", image: "https://pixabay.com/get/e837b1072af4003ed1584d05fb1d4e97e07ee3d21cac104497f8c378a7e9b0b9_340.jpg"},
+        {name: "Pitts Rest", image: "https://farm2.staticflickr.com/1363/1342367857_2fd12531e7.jpg"}
+    ];
 
 // add routes below
 app.get("/", function(req,res){
@@ -10,16 +17,23 @@ app.get("/", function(req,res){
 });
 
 app.get("/campgrounds", function(req, res){
-    var campgrounds = [
-        {name: "Salmon Creek", image: "https://pixabay.com/get/ec31b90f2af61c22d2524518b7444795ea76e5d004b014439df2c079a3e8b4_340.jpg"},
-        {name: "Loard Hill", image: "https://pixabay.com/get/e837b1072af4003ed1584d05fb1d4e97e07ee3d21cac104497f8c378a7e9b0b9_340.jpg"},
-        {name: "Pitts Rest", image: "https://farm2.staticflickr.com/1363/1342367857_2fd12531e7.jpg"}
-        ];
-        res.render("campgrounds", {campgrounds : campgrounds});
+    res.render("campgrounds", {campgrounds : campgrounds});
 });
 
+app.post("/campgrounds", function(req, res){
+    // get data from form 
+    var name = req.body.name;
+    var image = req.body.image;
+    var newCampground = {name: name, image: image};
+    //add to campgrounds array
+    campgrounds.push(newCampground);
+    // redirect back to campgrounds page
+    res.redirect("/campgrounds");
+});
 
-
+app.get("/campgrounds/new", function(req, res) {
+   res.render("new"); 
+});
 
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("YelpCamp Server Started!");
