f7c13e7cec7b30799b028dea88ccde0ebc63b248
Chirag <chirag.krishnaswamy@gmail.com>
2018-06-06 15:44:19 +0530

    Applying the concept of RESTful routes: * Added the "SHOW" route in app.js and also added the "description"   to each campground object. * Renamed "campgrounds.ejs" to "index.ejs" in coherence with the   RESTful routing nomenclature. * Updated "new.ejs" to allow users to type in a "description"   which is then posted via the POST route we set up earlier. * Created "show.ejs" that is the show page template that renders   to show details of a specific campground.

--- app.js
+++ app.js
@@ -8,63 +8,57 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs")
 
 // Schema setup
-
 var campgroundSchema = new mongoose.Schema({
     name : String,
-    image: String
+    image: String,
+    description: String
 });
-
 var Campground = mongoose.model("Campground", campgroundSchema);
 
 // Campground.create(
-//     {
-//         name: "Loard Hill", 
-//         image: "https://pixabay.com/get/e837b1072af4003ed1584d05fb1d4e97e07ee3d21cac104497f8c378a7e9b0b9_340.jpg"
-        
-//     }, function(err, campground) {
-//         if(err) {
-//             console.log(err);
-//         } else {
-//             console.log("NEWLY CREATED CAMPGROUND: ");
-//             console.log(campground);
-//         }
-//     });
+// {
+//     name: "Granite Hill", 
+//     image: "https://pixabay.com/get/e837b1072af4003ed1584d05fb1d4e97e07ee3d21cac104497f8c378a7e9b0b9_340.jpg",
+//     description: "This is a huge Granite Hill. No bathrooms. No water. Beautiful granite!"
+    
+// }, function(err, campground) {
+//     if(err) {
+//         console.log(err);
+//     } else {
+//         console.log("NEWLY CREATED CAMPGROUND: ");
+//         console.log(campground);
+//     }
+// });
 
-// var campgrounds = [
-//         {name: "Salmon Creek", image: "https://pixabay.com/get/ec31b90f2af61c22d2524518b7444795ea76e5d004b014439df2c079a3e8b4_340.jpg"},
-//         {name: "Loard Hill", image: "https://pixabay.com/get/e837b1072af4003ed1584d05fb1d4e97e07ee3d21cac104497f8c378a7e9b0b9_340.jpg"},
-//         {name: "Pitts Rest", image: "https://farm2.staticflickr.com/1363/1342367857_2fd12531e7.jpg"},
-//         {name: "Collin Woods", image: "https://farm2.staticflickr.com/1281/4684194306_18ebcdb01c.jpg"},
-//         {name: "Pitts Rest", image: "https://farm2.staticflickr.com/1363/1342367857_2fd12531e7.jpg"},
-//         {name: "Salmon Creek", image: "https://pixabay.com/get/ec31b90f2af61c22d2524518b7444795ea76e5d004b014439df2c079a3e8b4_340.jpg"},
-//         {name: "Collin Woods", image: "https://farm2.staticflickr.com/1281/4684194306_18ebcdb01c.jpg"},
-//         {name: "Loard Hill", image: "https://pixabay.com/get/e837b1072af4003ed1584d05fb1d4e97e07ee3d21cac104497f8c378a7e9b0b9_340.jpg"}
-//     ];
+// ADD ROUTES BELOW
 
-// add routes below
+// 'LANDING' ROUTE
 app.get("/", function(req,res){
     res.render("landing");
 });
 
+// 'INDEX' ROUTE
 app.get("/campgrounds", function(req, res){
     // retriving all campground data from database
     Campground.find({}, function(err, allCampgrounds){
        if(err){
            console.log(err);
        } else {
-            res.render("campgrounds", {campgrounds : allCampgrounds});
+            res.render("index", {campgrounds : allCampgrounds});
        }
     });
     // res.render("campgrounds", {campgrounds : campgrounds});
 });
 
+// 'CREATE' ROUTE
 app.post("/campgrounds", function(req, res){
     // get data from form 
     var name = req.body.name;
     var image = req.body.image;
+    var desc = req.body.description;
     
     //create new campground
-    var newCampground = {name: name, image: image};
+    var newCampground = {name: name, image: image, description: desc};
     
     // add to campgrounds array
     // campgrounds.push(newCampground);
@@ -83,8 +77,22 @@ app.post("/campgrounds", function(req, res){
     });
 });
 
+// 'NEW' ROUTE
 app.get("/campgrounds/new", function(req, res) {
    res.render("new"); 
+});
+
+// 'SHOW' ROUTE
+app.get("/campgrounds/:id", function(req, res) {
+    // find camp ground with provided ID
+    Campground.findById(req.params.id, function(err, foundCampground){
+        if(err){
+            console.log(err);
+        } else {
+            // render the show template with that campground
+            res.render("show", {campground: foundCampground});
+        }
+    });
 });
 
 app.listen(process.env.PORT, process.env.IP, function(){
