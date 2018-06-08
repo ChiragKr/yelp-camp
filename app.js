6b37b87a0c62ab1830c6ec1521db631a8801f27d
Chirag <chirag.krishnaswamy@gmail.com>
2018-06-09 00:43:58 +0530

    * Configured passport in app.js (app.use(...), passport.use(...)) * Updated app.js with the registration authorization route. * Created views/register.ejs i.e the form for a new user   to register.

--- app.js
+++ app.js
@@ -16,6 +16,18 @@ app.set("view engine", "ejs");
 seedDB();
 app.use(express.static(__dirname + "/public"));
 
+// PASSPORT CONFIG
+app.use(require("express-session")({
+    secret: "the ultimate truth is 72",
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 //=======================================================
 //                  CAMPGROUND ROUTES
 //=======================================================
@@ -117,6 +129,27 @@ app.post("/campgrounds/:id/comments",function(req, res){
         }
     });
     
+});
+
+// ======================================================
+//                    AUTH ROUTES
+// ======================================================
+
+app.get("/register", function(req, res) {
+   res.render("register"); 
+});
+
+app.post("/register", function(req, res) {
+    var newUser = new User({username: req.body.username});
+    User.register(newUser, req.body.password, function(err, user){
+      if(err){
+          console.log(err);
+          return res.render("/register");
+      }
+      passport.authenticate("local")(req, res, function(){
+          res.redirect("/campgrounds");
+      });
+    });
 });
 
 app.listen(process.env.PORT, process.env.IP, function(){
