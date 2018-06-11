a10518be8538b017fd6e031ab552d05ea6cbfe3a
Chirag <chirag.krishnaswamy@gmail.com>
2018-06-11 05:50:29 +0530

    ADD AUTHORIZATION TO COMMENTS SECTION * Update "routes/comments.js" by adding a middleware that allows   user to edit, update and delete a comment ONLY if they author it. * Update "views/campgrounds/show.ejs" to show "edit" and "delete"   buttons only on the comments the current user has authored.

--- routes/comments.js
+++ routes/comments.js
@@ -47,7 +47,7 @@ router.post("/", isLoggedIn, function(req, res){
 });
 
 // 'EDIT' ROUTE (comments)
-router.get("/:comment_id/edit", function(req, res) {
+router.get("/:comment_id/edit", checkCommentOwnership, function(req, res) {
     Comment.findById(req.params.comment_id, function(err, foundComment){
         if(err){
             res.redirect("back");
@@ -58,7 +58,7 @@ router.get("/:comment_id/edit", function(req, res) {
 });
 
 // 'UPDATE' ROUTE (comments)
-router.put("/:comment_id", function(req, res){
+router.put("/:comment_id", checkCommentOwnership, function(req, res){
     
     Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function(err, updatedComment){
         if(err){
@@ -70,7 +70,7 @@ router.put("/:comment_id", function(req, res){
 });
 
 // 'DESTROY' ROUTE (comments)
-router.delete("/:comment_id", function(req, res){
+router.delete("/:comment_id", checkCommentOwnership, function(req, res){
     Comment.findByIdAndRemove(req.params.comment_id, function(err){
         if(err){
             res.redirect("/campgrounds/"+ req.params.id);
@@ -86,6 +86,31 @@ function isLoggedIn(req, res, next){
        return next();
    } 
    res.redirect("/login");
+}
+
+function checkCommentOwnership (req, res, next){
+    // is user logged in
+    if(req.isAuthenticated()) {
+        Comment.findById(req.params.comment_id, function(err, foundComment){
+            if(err){
+                console.log(err);
+                res.redirect("back");
+            } else {
+                // does user own the comment
+                if(foundComment.author.id.equals(req.user._id)){
+                    next();
+                }
+                // otherwise, redirect
+                else {
+                    res.redirect("back");
+                }
+            }
+        });
+    }
+    // if not, redirect
+    else {
+        res.redirect("back");
+    } 
 }
 
 module.exports = router;
